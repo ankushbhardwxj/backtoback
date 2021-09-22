018002ba1c1f3c8725a891cd77947d170862e516
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2021-09-22 23:01:58 +0530

    django app

---
+++ VoteAppDjango/VoteApp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('VoteApp/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'VoteApp/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'VoteApp/detail.html', {'question': question })
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
