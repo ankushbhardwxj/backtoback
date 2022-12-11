775f0677c7c72088cb839c69e79f68652771bfa7
Ankush Bhardwaj <40923324+ankushbhardwxj@users.noreply.github.com>
2022-12-12 00:28:37 +0530

    added postgres config

--- infra/server/src/controllers/postgres-controller.ts
+++ infra/server/src/controllers/postgres-controller.ts
@@ -1,3 +1,25 @@
-const postgresController = async () => {};
+import { Client, Pool } from "pg";
+
+const postgresController = async () => {
+  const client = new Client({
+    user: "admin",
+    password: "admin",
+    port: 5432,
+    host: "postgres-svc",
+  });
+  await client.connect();
+  const res = await client.query("SELECT $1::text as message", [
+    "Hello world!",
+  ]);
+  console.log(res);
+  await client.end();
+  const pool = new Pool({
+    user: "admin",
+    password: "admin",
+    port: 5432,
+    host: "postgres-svc",
+  });
+  pool.query("SELECT $1::text as message", ["Hello world!"]);
+};
 
 export default postgresController;
